589fffa5177a69f998bf95af34e884e0bda88bfd
osmahi69 <smahioussama1@gmail.com>
2021-03-17 22:14:36 +0100

    first push

---
+++ components/hero/Hero.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hero is-medium is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          Bienvenu dans votre magasin LA KASBAH CHICHA
+        </h1>
+        <h2 class="subtitle">
+Magasin dediee à la decoration orientale moderne et aux chicha  A BORDEAUX       </h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+    .hero {
+    background: url('../../static/alkasbah.jpg') no-repeat;
+    background-position: 50% 50%;
+    background-size: 2065px;
+    margin: 20px;
+    }
+    h1, h2 {
+      color: #fff;
+    }
+</style>
